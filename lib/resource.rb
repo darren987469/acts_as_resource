2e6e157c88e6efd56346dac02a4fdc6201382189
darren.chang <darren987469@gmail.com>
2018-04-28 21:30:56 +0800

    init commit

---
+++ lib/resource.rb
@@ -0,0 +1,5 @@
+require "resource/version"
+
+module Resource
+  # Your code goes here...
+end
---
+++ spec/resource_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Resource do
+  it "has a version number" do
+    expect(Resource::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end

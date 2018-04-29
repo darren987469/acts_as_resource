b2f9b03454b5f2bc7733aed2bb34115cf97545f6
darren.chang <darren987469@gmail.com>
2018-04-29 09:35:28 +0800

    add migration template for generator

---
+++ lib/generators/resource/migration/templates/migration.rb
@@ -0,0 +1,23 @@
+class ResourceMigration < Resource::MIGRATION_BASE_CLASS
+  def self.up
+    create_table Resourceship, force: true, options: create_options do |t|
+      t.references :accessable, polymorphic: { null: false }
+      t.references :resourceable, polymorphic: { null: false }
+    end
+
+    add_index Resourceship, [:accessable_id, :accessable_type, :resourceable_id, :resourceable_type], name: 'relationships_accesable_resourceable_index', unique: true
+  end
+
+  def self.down
+    drop_table Resourceship
+  end
+
+  def self.create_options
+    options = ''
+    if defined?(ActiveRecord::ConnectionAdapters::Mysql2Adapter) \
+      && ActiveRecord::Base.connection.instance_of?(ActiveRecord::ConnectionAdapters::Mysql2Adapter)
+      options = 'DEFAULT CHARSET=latin1'
+    end
+    options
+  end
+end
